31598764d68b58a403b97f8b4419df251b37b0b7
河田聡 <wasurenai8888@gmail.com>
2024-10-21 01:20:32 +0900

    Update abc227-b.rb

--- abc227-b.rb
+++ abc227-b.rb
@@ -1,1 +1,10 @@
-N=gets.to_i
+l = []
+1.upto(200){|a|
+	1.upto(200){|b|
+		l.push(4 * a * b + 3 * a + 3 * b)
+	}
+}
+l.uniq!.delete_if{|x|x > 1000}
+gets
+s = gets.split.map(&:to_i)
+puts s.count{|x|!l.include?(x)}
